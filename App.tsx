c7ddf1bd7f7ce387f647cc8ca0dab8bdb63b14ff
Revanth Kumar <revanth.kumar@colortokens.com>
2021-01-26 11:28:06 +0530

    smartshare init

--- App.tsx
+++ App.tsx
@@ -36,12 +36,9 @@ const App = () => {
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
+          <View>
+            <Text>Smartshare</Text>
+          </View>
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Step One</Text>
